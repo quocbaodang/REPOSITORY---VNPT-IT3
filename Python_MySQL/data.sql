377a0d6fa01bfe44c5b13749961526ba3e1a0c0d
QuocBaoDang <dangquocbaobk92@gmail.com>
2022-07-08 21:15:57 +0700

    Upload Python_MySQL

--- Python_MySQL/data.sql
+++ Python_MySQL/data.sql
@@ -11,3 +11,9 @@ create table Info
 	section nvarchar(100),
 )
 go
+
+insert into Info (name, class, section) values (N'Quoc Bao Dang', N'A', N'1th')
+insert into Info (name, class, section) values (N'Ronaldo', N'B', N'2th')
+insert into Info (name, class, section) values (N'Messi', N'E', N'5th')
+insert into Info (name, class, section) values (N'Neymar', N'A', N'4th')
+go
